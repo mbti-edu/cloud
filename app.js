387224aa888945d022fd369556777bb6f2ac803d
Syifa Layla <125670543+syifalayla@users.noreply.github.com>
2023-06-03 18:24:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,12 +11,10 @@ app.use(bodyParser.json());
 // Import routes
 const authRoutes = require('./routes/auth');
 const mbtiRoutes = require('./routes/mbti');
-const homepageRoutes = require('./routes/homepage');
 
 // Use routes
 app.use('/auth', authRoutes);
 app.use('/mbti', mbtiRoutes);
-app.use('/homepage', homepageRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
