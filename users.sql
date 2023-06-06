957f46bc9ee30d03fa833befc6b7c37e6f51d735
Firda <125743836+firdanrl@users.noreply.github.com>
2023-06-06 20:54:50 +0700

    Update users.sql

--- users.sql
+++ users.sql
@@ -3,48 +3,41 @@
 create database mbti_edu;
 
 --GROUP: Authorization
---Tabel "User" digunakan untuk menyimpan data pengguna yang melakukan registrasi.
-CREATE TABLE User (
+--Tabel "user" digunakan untuk menyimpan data pengguna yang melakukan registrasi.
+CREATE TABLE user (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(255),
-  email VARCHAR(255) UNIQUE,
-  password VARCHAR(255),
-  gender VARCHAR(255),
+  name VARCHAR(50) not null,
+  email VARCHAR(50) UNIQUE,
+  password VARCHAR(20) not null,
+  gender VARCHAR(10) not null,
   birthdate DATE
 );
 
+-- data dump tabel user
+insert into user values 
+(1, 'example', 'example123@gmail.com', 'example123', 'female', '1990-01-01');
+
 --Tabel "Session" digunakan untuk menyimpan informasi sesi login pengguna, termasuk token akses.
-CREATE TABLE Session (
+CREATE TABLE session (
   id INT PRIMARY KEY AUTO_INCREMENT,
   userId INT,
   token VARCHAR(255),
-  FOREIGN KEY (userId) REFERENCES User(id)
+  FOREIGN KEY (userId) REFERENCES user(id)
 );
+
+-- data dump session
+insert into session values (
+  1, 1, 'abcde12345');
 
 --GROUP: MBTI
 --Tabel "Result" digunakan untuk menyimpan hasil tes MBTI yang telah dijalankan.
-CREATE TABLE Result (
+CREATE TABLE result (
   id INT PRIMARY KEY AUTO_INCREMENT,
   userId INT,
-  mbti VARCHAR(255),
-  result VARCHAR(255),
-  FOREIGN KEY (userId) REFERENCES User(id)
+  mbti VARCHAR(6),
+  FOREIGN KEY (userId) REFERENCES user(id)
 );
 
---GROUP: Homepage
---Tabel "Dashboard" digunakan untuk menyimpan data dashboard pengguna, seperti data MBTI dan deskripsi kepribadian.
-CREATE TABLE Dashboard (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(255),
-  mbti VARCHAR(255),
-  kepribadian VARCHAR(255),
-  deskripsi TEXT
-);
-
---Tabel "Recommendations" digunakan untuk menyimpan rekomendasi berdasarkan hasil tes pengguna.
-CREATE TABLE Recommendations (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(255),
-  mbti VARCHAR(255),
-  recommendations string
-);
+-- data dump result
+insert into result values (
+  1, 1, 'INFP');
