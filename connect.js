94ac36196cf6d85670bee50eca0832bc03cc20f2
Firda <125743836+firdanrl@users.noreply.github.com>
2023-06-06 21:03:36 +0700

    Create connect.js

---
+++ connect.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+var mysql = require('mysql')
+
+const con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+})
+
+con.connect((err)=>{
+    if(err) throw err
+    console.log('MySQL is connected')
+})
+
+module.exports = connect;
