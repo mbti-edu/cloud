5f7b0322b24bc849e8bddb0e5b41fa4dd10d85dd
Syifa Layla <125670543+syifalayla@users.noreply.github.com>
2023-06-08 20:59:19 +0700

    Update handlers.js

--- handlers.js
+++ handlers.js
@@ -1,20 +1,10 @@
-// Handler for register endpoint
-exports.registerHandler = (req, res) => {
-    // Handle registration logic
-  };
-  
-  // Handler for login endpoint
-  exports.loginHandler = (req, res) => {
-    // Handle login logic
-  };
-  
-  // Handler for test page endpoint
-  exports.testPageHandler = (req, res) => {
-    // Handle test page logic
-  };
-  
-  // Handler for result page endpoint
-  exports.resultPageHandler = (req, res) => {
-    // Handle result page logic
-  };
-  
+require("dotenv").config();
+
+
+// Ekspor handler ke file server.js atau file lainnya
+module.exports = {
+  registerHandler,
+  loginHandler,
+  testPageHandler,
+  resultPageHandler,
+};
