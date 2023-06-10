d0d0429dc08620ab72aa118c2f619246926f053a
Syifa Layla <125670543+syifalayla@users.noreply.github.com>
2023-06-10 15:46:31 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const connection = require("./connect");
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
