469dbc97490cb37429a9a09d6dcc66e5b05d2495
Firda <125743836+firdanrl@users.noreply.github.com>
2023-06-10 18:52:20 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const connection = require("./connect");
+const connection = require("src/connect");
 
 const app = express();
 const port = 8080;
