6e140e47410ab44cc3cc2e8c4ad1bf3a06b93f6d
Syifa Layla <125670543+syifalayla@users.noreply.github.com>
2023-06-13 15:52:31 +0700

    Create handlers.js

---
+++ handlers.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+
+// Ekspor handler ke file server.js atau file lainnya
+module.exports = {
+  registerHandler,
+  loginHandler,
+  testPageHandler,
+  resultPageHandler,
+  saveResultHandler,
+};
